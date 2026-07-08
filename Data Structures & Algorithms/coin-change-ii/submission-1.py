03e4c6abc37ef26cc9d6f5dc3223abf2f9f88c68
Kartikay Kukreja <kukrejakartikay@gmail.com>
2026-07-08 14:44:38 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        ways=[0]*(amount+1)
+        dp=[float('inf')]*(amount+1)
+        dp[0]=1
+        ways[0]=1
+        
+        for c in coins:
+            for i in range(amount):
+                if(i+c<=amount):
+                    ways[i+c]+=ways[i]
+        return ways[amount]
+     
+
+    
+        
+        
